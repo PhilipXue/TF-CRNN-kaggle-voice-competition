8da6879605030c9fede71cd426114b9c4e997d05
Dongyi Xue <dongyi.xue@sbibits.com>
2018-05-01 14:33:22 +0900

    change `os` reliance to pathlib.path

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import platform
-import os
+from pathlib import Path
 if platform.system() == 'Linux':
     data_root = Path('/home/philip/data/Keyword_spot')
 else:
@@ -7,10 +7,8 @@ else:
 training_audio_data_folder = data_root / 'train/audio'
 training_audio_data = training_audio_data_folder.glob("*/*.wav")
 training_img_data_folder = data_root / 'train/trian_image'
-if not training_img_data_folder.exists():
-    os.mkdir(training_img_data_folder)
+training_img_data_folder.mkdir(parents=True, exist_ok=True)
 test_audio_data_folder = data_root / 'test/audio'
 test_audio_data = test_audio_data_folder.glob("*.wav")
 test_image_data_folder = data_root / 'test/test_image'
-if not test_image_data_folder.exists():
-    os.mkdir(test_image_data_folder)
+test_image_data_folder.mkdir(parents=True, exist_ok=True)
